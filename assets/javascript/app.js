075e557ebc406b5ebab99d4112480a6bf14b8f51
Armmand Alston <ArmmandAlston33@gmail.com>
2017-03-26 02:12:29 -0400

    created content in  html file

---
+++ assets/javascript/app.js
@@ -0,0 +1,59 @@
+function check() {
+
+	var question1= document.quiz.question1.value;
+	var question2= document.quiz.question2.value; 
+	var question3= document.quiz.question3.value; 
+	var question4= document.quiz.question4.value; 
+	var question5= document.quiz.question5.value; 
+	var correct= 0;
+
+		if (question1 == "Age 19"){
+			correct++;
+
+	}
+	    if (question2 == "201"){
+	    	correct++;
+
+	} 
+	    if (question3 == "1") {
+	    	correct++;
+
+	}
+	    if (question4 == "Jelly Bean"){
+	    	correct++;
+
+	}
+	    if (question5 == "18 years"){
+	    	correct++;
+
+	}
+
+
+
+  document.getElementById("after_submit").style.visibility = "visible";
+  document.getElementById("number_correct").innerHTML= "You got out of 5 " + correct + " correct.";
+}
+
+var sec = 40;
+var myTimer = document.getElementById('myTimer');
+var myBtn = document.getElementById('myBtn');
+window.onload = countDown;
+
+function countDown() {
+  if (sec < 10) {
+    myTimer.innerHTML = "0" + sec;
+  } else {
+    myTimer.innerHTML = sec;
+  }
+  if (sec <= 0) {
+    $("#myBtn").removeAttr("disabled");
+    $("#myBtn").removeClass().addClass("btnEnable");
+    $("#myTimer").fadeTo(2500, 0);
+    myBtn.innerHTML = "Time is up!";
+    return;
+  }
+  sec -= 1;
+  window.setTimeout(countDown, 1000);
+} 
+	  
+ 
